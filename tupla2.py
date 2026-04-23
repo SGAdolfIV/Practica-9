ed0158292d4e1fd39d6dced1dbf011ef427aa6a3
SGAdolfIV <adolfosalga02@gmail.com>
2026-04-23 16:42:48 +0000

    gdjd

--- tupla2.py
+++ tupla2.py
@@ -1,7 +1,7 @@
-diasdemes =(("Enero", 31), ("Febrero", 28), ("Marzo", 31), ("Abril", 30), ("Mayo", 31), 
+diasdeMes =(("Enero", 31), ("Febrero", 28), ("Marzo", 31), ("Abril", 30), ("Mayo", 31), 
             ("Junio", 30), ("Julio", 31), ("Agosto", 31), ("Septiembre", 30), ("Octubre", 31), ("Noviembre", 30), ("Diciembre", 31))
 print(diasdeMes)
-print("{} tiene {} días".format(diasdemes[0][0], diasdemes[0][1]))
-print("{} tiene {} días".format(diasdemes[6][0], diasdemes[6][1]))
-print("{} tiene {} días".format(diasdemes[7][0], diasdemes[7][1]))
-print("{} tiene {} días".format(diasdemes[11][0], diasdemes[11][1]))
+print("{} tiene {} días".format(diasdeMes[0][0], diasdeMes[0][1]))
+print("{} tiene {} días".format(diasdeMes[6][0], diasdeMes[6][1]))
+print("{} tiene {} días".format(diasdeMes[7][0], diasdeMes[7][1]))
+print("{} tiene {} días".format(diasdeMes[11][0], diasdeMes[11][1]))
